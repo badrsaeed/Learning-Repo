8754ed69efe222090dfe478f2cb587bbd209f718
Badr Saeed <bsaeed@arpuplus.com>
2024-02-24 18:38:39 +0200

    ''

---
+++ DSandAlgorithms/SortingAlgorithms/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class Employee : IComparable<Employee>
+    {
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public int CompareTo(Employee? other)
+        {
+            //if(this.Salary > other?.Salary)
+            //    return 1;
+            //else if(this.Salary < other?.Salary)
+            //    return -1;
+            //else return 0;
+            return Salary.CompareTo(other?.Salary);
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Name} - {Salary}";
+        }
+    }
+}
---
+++ DSandAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,41 @@
+﻿using SortingAlgorithms.Sorting_Services;
+
+namespace SortingAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Selection Sort
+            //NUMBERs
+            //int[] numbers = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+            //SelectionSort.Sort(numbers);
+            //Console.WriteLine(string.Join(',', numbers));
+
+            //STRINGS
+            //string[] strings = { "Badr", "Saeed", "Ali", "Mohammed", "Hassan" };
+            //SelectionSort.Sort(strings);
+            //Console.WriteLine(string.Join(',', strings)); 
+
+            //Custom Object
+            //Employee[] emplyees =
+            //{
+            //    new Employee(1,"Badr", 15000),
+            //    new Employee(2,"Saeed",  1500),
+            //    new Employee(3,"Ali", 5000),
+            //    new Employee(4,"Mohamed",1000),
+            //};
+
+            //SelectionSort.Sort(emplyees);
+
+            //foreach (Employee emp in emplyees)
+            //{
+            //    Console.WriteLine(emp.ToString());
+            //}
+            #endregion
+
+
+        }
+    }
+}
